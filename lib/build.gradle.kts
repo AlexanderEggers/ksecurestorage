564a514dff232d3fe9d1c5f6074f8e92cdc2095f
Alexander Eggers <7427545+AlexanderEggers@users.noreply.github.com>
2024-04-02 04:22:25 +1100

    ci: update deployment workflow

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "io.github.alexandereggers"
-version = "0.0.2"
+version = (System.getenv("RELEASE_TAG_NAME") ?: "v0.0.0-local").replace("v", "")
 
 kotlin {
     androidTarget {
