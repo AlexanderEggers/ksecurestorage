53634ef13bd51e10a6a6fb408d9f747b71f6c8e0
Alexander Eggers <7427545+AlexanderEggers@users.noreply.github.com>
2024-04-08 21:50:44 +1000

    feat: implement ios

--- lib/src/iosMain/kotlin/io/github/alexandereggers/ksecurestorage/KSecureStorage.ios.kt
+++ lib/src/iosMain/kotlin/io/github/alexandereggers/ksecurestorage/KSecureStorage.ios.kt
@@ -1,20 +1,88 @@
 package io.github.alexandereggers.ksecurestorage
 
-actual class KSecureStorage actual constructor(): IKSecureStorage {
+import kotlinx.cinterop.*
+import platform.CoreFoundation.*
+import platform.Foundation.*
+import platform.Security.*
+
+@OptIn(ExperimentalForeignApi::class, BetaInteropApi::class)
+actual class KSecureStorage actual constructor() : IKSecureStorage {
 
     override fun setItem(key: String, value: String?) {
-        TODO("Not yet implemented")
+        val query = query(
+            kSecClass to kSecClassGenericPassword,
+            kSecAttrAccount to CFBridgingRetain(key),
+            kSecValueData to CFBridgingRetain(value)
+        )
+
+        SecItemDelete(query)
+        val status = SecItemAdd(query, null)
+
+        if (status == errSecDuplicateItem) {
+            throw Exception("Item with key $key already exists")
+        } else if (status != errSecSuccess) {
+            throw Exception("Error adding item with key $key")
+        }
     }
 
     override fun getItem(key: String): String? {
-        TODO("Not yet implemented")
+        val getQuery = query(
+            kSecClass to kSecClassGenericPassword,
+            kSecAttrAccount to CFBridgingRetain(key),
+            kSecReturnData to kCFBooleanTrue,
+            kSecMatchLimit to kSecMatchLimitOne
+        )
+
+        memScoped {
+            val result = alloc<CFTypeRefVar>()
+            val status = SecItemCopyMatching(getQuery, result.ptr)
+
+            if (status == errSecItemNotFound) {
+                return null
+            } else if (status != errSecSuccess) {
+                throw Exception("Error getting item with key $key")
+            } else {
+                val data = CFBridgingRelease(result.value) as NSData
+                return NSString.create(data, NSUTF8StringEncoding).toString()
+            }
+        }
     }
 
     override fun removeItem(key: String) {
-        TODO("Not yet implemented")
+        val deleteQuery = query(
+            kSecClass to kSecClassGenericPassword,
+            kSecAttrAccount to CFBridgingRetain(key),
+            kSecReturnData to kCFBooleanTrue,
+        )
+
+        val status = SecItemDelete(deleteQuery)
+
+        if (status != errSecSuccess) {
+            throw Exception("Error deleting item with key $key and status $status")
+        }
     }
 
     override fun clear() {
-        TODO("Not yet implemented")
+        val query = query(
+            kSecClass to kSecClassGenericPassword
+        )
+
+        val status = SecItemDelete(query)
+
+        if (status != errSecSuccess) {
+            throw Exception("Error clearing items with status $status")
+        }
+    }
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private fun query(vararg pairs: Pair<CFStringRef?, CFTypeRef?>): CFMutableDictionaryRef? {
+    val map = mapOf(*pairs)
+    return CFDictionaryCreateMutable(
+        null, map.size.convert(), null, null
+    ).apply {
+        map.forEach { CFDictionaryAddValue(this, it.key, it.value) }
+    }.apply {
+        CFAutorelease(this)
     }
 }
